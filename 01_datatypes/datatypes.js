0c38be048908800806b209a75c90e664ad587815
anuargdiwakar <145633819+anuargdiwakar@users.noreply.github.com>
2023-11-03 10:42:19 +0000

    complete

--- 01_datatypes/datatypes.js
+++ 01_datatypes/datatypes.js
@@ -1,2 +1,14 @@
-console.log("anurag")
-console.log("happy")
+// console.log("anurag")
+// console.log("happy") 
+
+const accountId = 12234
+let accountname = "Anurag"
+let accountemailid = "anuragdiwalar5@gmail.com"
+let accountmobilenumber = "3216549871"
+
+console.log("accountId");
+console.log("accountname");
+console.log("accountemailid");
+console.log("accountmobilenumber");
+
+console.table([accountId, accountname, accountmobilenumber, accountemailid])
