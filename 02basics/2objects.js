a4462ae12c26504a3815e7d45ced7d455befc762
anuargdiwakar <145633819+anuargdiwakar@users.noreply.github.com>
2023-11-16 14:33:29 +0000

    done

---
+++ 02basics/2objects.js
@@ -0,0 +1,20 @@
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "sammy"
+tinderUser.isloggedin = false
+
+//console.log(tinderUser);
+
+const regularuser = {
+    email : "anuragdiwakar@gmail.com"
+    fullname : {
+        userfullname : {
+            firstname:"anurag",
+            lastname:"diwakar"
+
+        }
+    }
+}
+
+console.log(regularuser.fullname.userfullname.firstname);
