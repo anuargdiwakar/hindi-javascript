6e9752242787b946395a986d53de3824e61c19b7
anuargdiwakar <145633819+anuargdiwakar@users.noreply.github.com>
2023-11-16 14:51:19 +0000

    complete

--- 02basics/2objects.js
+++ 02basics/2objects.js
@@ -7,7 +7,7 @@ tinderUser.isloggedin = false
 //console.log(tinderUser);
 
 const regularuser = {
-    email : "anuragdiwakar@gmail.com"
+    email : "anuragdiwakar@gmail.com" ;
     power: {
         userfullname : 
         {
@@ -18,4 +18,11 @@ const regularuser = {
     }
 }
 
-console.log(regularuser.power.userfullname.firstname);
+//console.log(regularuser.power.userfullname.firstname);
+
+const obj1 = {1: "a" , 2: "b"}
+const obj2 = {3: "a" , 4: "b"}
+
+const obj3={obj1,obj2}
+
+console.log(obj3);
