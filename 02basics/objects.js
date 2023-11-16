c2c59ad12d799784d623833f1286b1deaeb5ef0e
anuargdiwakar <145633819+anuargdiwakar@users.noreply.github.com>
2023-11-16 10:41:10 +0000

    complete

---
+++ 02basics/objects.js
@@ -0,0 +1,11 @@
+const jsuser = {
+    name : "anurg diwakar" ,
+    [mySym] : "mykey1" ,
+    age : 18 ,
+    location : "lucknow" ,
+    email : "anuragdiwakar5@gmail.com",
+    isLoggedIn : false ,
+
+}
+
+console.log(jsuser["location"]);
